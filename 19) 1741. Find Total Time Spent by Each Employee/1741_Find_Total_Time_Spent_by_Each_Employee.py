cbc45fb219551d6227024dc9b68e36be293e9576
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2023-09-01 05:22:23 +0300

    19th Problem Find Total Time Spent By Each Employee Solution with code and Full Explanation

---
+++ 19) 1741. Find Total Time Spent by Each Employee/1741_Find_Total_Time_Spent_by_Each_Employee.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def total_time(employees: pd.DataFrame) -> pd.DataFrame:
+
+    employees['total_time'] = employees['out_time'] - employees['in_time']
+    return employees.groupby(['event_day', 'emp_id'])['total_time'].agg('sum').reset_index().rename(columns={'event_day' : 'day'})
