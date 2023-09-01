decdefc3579989c30114b04b7d09f815c92564c5
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2023-09-01 05:39:17 +0300

    27th problem 1378. Replace Employee ID With The Unique Identifier Problem Solution with code and full explanation

---
+++ 27) 1378. Replace Employee ID With The Unique Identifier/1378_Replace_Employee_ID_With_The_Unique_Identifier.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+
+    return employees.merge(employee_uni, on='id', how='left').drop('id', axis=1)
